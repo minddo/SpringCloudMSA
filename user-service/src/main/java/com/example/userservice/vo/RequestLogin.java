8f96e51880c1cf6dda5a2af417fdf3518b15ce38
minddo <mindrome95@kaist.ac.kr>
2021-09-13 19:06:48 +0900

    authentication filter 추가

--- user-service/src/main/java/com/example/userservice/vo/RequestLogin.java
+++ user-service/src/main/java/com/example/userservice/vo/RequestLogin.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Size;
 
 @Data
 public class RequestLogin {
-
+    //로그인데이터 받는곧
     @NotNull(message = "Email can not be null")
     @Size(min = 2, message = "Email not be less than two characters")
     @Email
@@ -17,4 +17,6 @@ public class RequestLogin {
     @NotNull(message = "Password can not be null")
     @Size(min = 8, message = "Password must be equal or greater than eight characters")
     private String password;
+
+
 }
