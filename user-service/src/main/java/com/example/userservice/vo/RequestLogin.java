3db99ac54aa0d4fc11dcf1e76dc7e8d8c0bebf60
mindroem95@kaist.ac.kr <mindrome95@gmail.com>
2021-09-13 15:50:57 +0900

    authentication filter 추가

---
+++ user-service/src/main/java/com/example/userservice/vo/RequestLogin.java
@@ -0,0 +1,20 @@
+package com.example.userservice.vo;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class RequestLogin {
+
+    @NotNull(message = "Email can not be null")
+    @Size(min = 2, message = "Email not be less than two characters")
+    @Email
+    private String email;
+
+    @NotNull(message = "Password can not be null")
+    @Size(min = 8, message = "Password must be equal or greater than eight characters")
+    private String password;
+}
